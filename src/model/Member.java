2cff5be51c37306ec681e94cd364cdbcdf278860
misshanhit <misshanhit@gmail.com>
2014-07-14 11:04:18 +0700

    Test get new file
    Test get new file

---
+++ src/model/Member.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Member {
+	private long id;
+	private String firstName;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+}
